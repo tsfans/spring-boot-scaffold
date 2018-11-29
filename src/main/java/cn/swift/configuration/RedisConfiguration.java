92b35a548db779e873fc867c13d5243a2650f6f5
Swift <hu382018@outlook.com>
2018-11-29 16:42:57 +0800

    修正redis配置错误.

--- src/main/java/cn/swift/configuration/RedisConfiguration.java
+++ src/main/java/cn/swift/configuration/RedisConfiguration.java
@@ -1,20 +1,24 @@
 package cn.swift.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 
 @Configuration
-public class Redisconfiguration {
+public class RedisConfiguration {
+    
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate() {
     GenericFastJsonRedisSerializer serializer = new GenericFastJsonRedisSerializer();
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
-    redisTemplate.setConnectionFactory(new JedisConnectionFactory());
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
     redisTemplate.setDefaultSerializer(serializer);
     redisTemplate.afterPropertiesSet();
     return redisTemplate;
